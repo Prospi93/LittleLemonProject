b131f6df9652675898374b94e7c42f92ea981715
Federico Prospero <prospy93@icloud.com>
2023-07-23 17:57:07 +0200

    layout improvements

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,11 @@
+import Tavolo from './assets/Tavolo.jpg';
+
 export default function Main() {
     return (
         <main>
             <div className="containerMain">
                 <h2>Reserve a table page</h2>
+                <img src={Tavolo} alt="Little Lemon table"></img>
             </div>
         </main>
     );
